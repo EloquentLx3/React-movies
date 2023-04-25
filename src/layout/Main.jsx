c755754db2db16cde9ba53f8a5863a4b0b9daa15
EloquentLx3 <mikedeerebestinallworld2012@gmail.com>
2023-04-25 16:10:22 +0300

    update link

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -4,6 +4,7 @@ import { Preloader } from "../Components/Preloader";
 import { Search } from "../Components/Search";
 
 const API_KEY = process.env.REACT_APP_API_KEY;
+console.log(process.env.REACT_APP_API_KEY);
 
 class Main extends Component {
   state = {
@@ -12,22 +13,35 @@ class Main extends Component {
   };
 
   componentDidMount() {
+    console.log(process.env.REACT_APP_API_KEY);
     fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=matrix`)
       .then((response) => response.json())
-      .then((data) => this.setState({ movies: data.Search, loading: false }));
+      .then((data) => this.setState({ movies: data.Search, loading: false }))
+      .catch((err) => {
+        console.error(err);
+        this.setState({ loading: false });
+      });
   }
 
   searchMovies = (str) => {
     this.setState({ loading: true });
     fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${str}`)
       .then((response) => response.json())
-      .then((data) => this.setState({ movies: data.Search, loading: false }));
+      .then((data) => this.setState({ movies: data.Search, loading: false }))
+      .catch((err) => {
+        console.error(err);
+        this.setState({ loading: false });
+      });
   };
 
   filterMovies = (str, type) => {
     fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${str}&type=${type}`)
       .then((response) => response.json())
-      .then((data) => this.setState({ movies: data.Search }));
+      .then((data) => this.setState({ movies: data.Search }))
+      .catch((err) => {
+        console.error(err);
+        this.setState({ loading: false });
+      });
   };
 
   render() {
